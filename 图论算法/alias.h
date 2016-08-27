239439c8bc31c60f5307f95655308ad055010757
zhanglanqing <1174514176@qq.com>
2016-08-27 19:38:15 +0800

    Update this repo with CLRS

--- 图论算法/alias.h
+++
@@ -1,8 +0,0 @@
-#define Iterable vector
-#define Integer int
-#define boolean bool
-#define adj Adj
-#define isEmpty isempty
-#define Stack stack
-#define RST(x) memset(x,0,sizeof(x))
-//#define INFINITY 0x3f3f3f3f
